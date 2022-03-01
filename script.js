66c8b2db48023db6aa982d7a4fb10b598ca8578b
“CreamToby” <hyson09@gmail.com>
2022-03-01 15:49:59 -0500

    VariablesExercise

---
+++ script.js
@@ -0,0 +1,51 @@
+// 1
+// const
+
+// 2
+let num = 20;
+// console.log(num);  //20
+
+// 3
+num = 15;
+// console.log(num);  //15
+
+// 4
+num = num + 7;
+// console.log(num);  // value +7
+
+// 5
+num --;
+// console.log(num);  // value -1
+
+// 6
+num = "This is not a number"
+// console.log(num); //This is not a number
+
+// 7
+let blank ;
+// 8
+console.log(blank);  //undefined
+
+// 9
+blank = null;
+console.log(blank);  //null
+
+
+// BONUS    
+// 1
+const quarter = 0.25, dime = 0.10, nickel = 0.05, penny = 0.01;
+
+// 2
+console.log(quarter); // 0.25
+console.log(dime); // 0.1
+console.log(nickel);  // 0.05
+console.log(penny); // 0.01
+
+// 3
+let letters = "qwertyuiop"
+// console.log(letters); // qwertyuiop
+
+// 4
+console.log(letters.length);  // 10
+console.log(letters.slice(9)); // remember this starts 0
+
