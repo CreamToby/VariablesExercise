cce62c220fd3cabe209cff3ca4538b666305198c
“CreamToby” <hyson09@gmail.com>
2022-03-02 10:05:56 -0500

    update answer

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ num = 15;
 
 // 4
 num = num + 7;
+// num += 7;  //shorthand
 // console.log(num);  // value +7
 
 // 5
@@ -18,7 +19,7 @@ num --;
 // console.log(num);  // value -1
 
 // 6
-num = "This is not a number"
+num = "This is not a number";
 // console.log(num); //This is not a number
 
 // 7
@@ -28,18 +29,22 @@ console.log(blank);  //undefined
 
 // 9
 blank = null;
-console.log(blank);  //null
+// console.log(blank);  //null
 
 
 // BONUS    
 // 1
 const quarter = 0.25, dime = 0.10, nickel = 0.05, penny = 0.01;
 
+// const [quarter, dime, nickel, penny] = [0.25, 0.10, 0.05, 0.01];
+
 // 2
-console.log(quarter); // 0.25
-console.log(dime); // 0.1
-console.log(nickel);  // 0.05
-console.log(penny); // 0.01
+// console.log(quarter); // 0.25
+// console.log(dime); // 0.1
+// console.log(nickel);  // 0.05
+// console.log(penny); // 0.01
+
+console.log(quarter, dime, nickel, penny);
 
 // 3
 let letters = "qwertyuiop";
@@ -47,5 +52,11 @@ let letters = "qwertyuiop";
 
 // 4
 console.log(letters.length);  // 10
-console.log(letters.slice(9)); // remember this starts 0
+
+console.log(letters[9]);
+
+// console.log(letters.slice(9)); // remember this starts 0
+// console.log(letter.slice(-1));
+// console.log(letters.length(9));
+console.log(letters[letters. length - 1]);  //It's not working when you didn't comment out above // console.log(letters.length(9));
 
